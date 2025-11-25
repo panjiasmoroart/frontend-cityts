b3bb59a6b1805053f3d83828ed633353820d3709
panjiasmoroart <pandjiasmoroart@gmail.com>
2025-11-25 13:46:02 +0700

    Types - Permission

---
+++ src/types/permission.ts
@@ -0,0 +1,32 @@
+// --- Interface Permission
+export interface Permission {
+  id: number;
+  name: string;
+  created_at: string;
+  updated_at: string;
+}
+
+// --- Struktur respons API untuk list Permission (pagination)
+export interface PermissionsResponse {
+  current_page: number;
+  data: Permission[];
+  last_page: number;
+  total: number;
+  per_page: number;
+  next_page_url: string | null;
+  prev_page_url: string | null;
+  from: number;
+  to: number;
+  path: string;
+}
+
+// --- Interface untuk request membuat Permission
+export interface PermissionCreateRequest {
+  name: string;
+}
+
+// --- Interface untuk request update Permission
+export interface PermissionUpdateRequest {
+  id: number;
+  name: string;
+}
