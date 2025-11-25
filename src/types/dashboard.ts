d06f39a0b8795b89a9520729acc2ce51e9197444
panjiasmoroart <pandjiasmoroart@gmail.com>
2025-11-25 16:23:36 +0700

    Types - Dashboard

---
+++ src/types/dashboard.ts
@@ -0,0 +1,7 @@
+// --- Struktur respons API Dashboard
+export interface DashboardResponse {
+  categories_count: number;
+  posts_count: number;
+  products_count: number;
+  aparaturs_count: number;
+}
