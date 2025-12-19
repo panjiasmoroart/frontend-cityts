74a465799f6b9a9a4eb50d8c1e6844b7be8d99ce
panjiasmoroart <pandjiasmoroart@gmail.com>
2025-12-19 09:12:46 +0700

    Post Hooks - Membuat Hook Post (Mutation)

---
+++ src/hooks/admin/post/usePostUpdate.tsx
@@ -0,0 +1,40 @@
+// import useMutation dari '@tanstack/react-query';
+import { useMutation } from "@tanstack/react-query";
+
+// import service API
+import Api from "../../../services/api";
+
+// import type
+import type { PostUpdateRequest } from "../../../types/post";
+
+// import js-cookie
+import Cookies from "js-cookie";
+
+// Custom hook untuk update Post
+export const usePostUpdate = () => {
+  return useMutation({
+    // Mutation function untuk update post
+    mutationFn: async (data: PostUpdateRequest) => {
+      const token = Cookies.get("token");
+
+      // Buat FormData
+      const formData = new FormData();
+      if (data.image) {
+        formData.append("image", data.image);
+      }
+      formData.append("category_id", data.category_id.toString());
+      formData.append("title", data.title);
+      formData.append("content", data.content);
+
+      // Kirim request PUT/POST ke endpoint update
+      const response = await Api.put(`/api/admin/posts/${data.id}`, formData, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "multipart/form-data",
+        },
+      });
+
+      return response.data;
+    },
+  });
+};
