fa11426aaa2e7409e1c5b9874e2c18252fdad623
panjiasmoroart <pandjiasmoroart@gmail.com>
2025-12-19 22:23:35 +0700

    Product Hooks - Membuat Hook Product Delete (Mutation)

---
+++ src/hooks/admin/product/useProductDelete.tsx
@@ -0,0 +1,29 @@
+// import useMutation dari '@tanstack/react-query';
+import { useMutation } from "@tanstack/react-query";
+
+// import service API
+import Api from "../../../services/api";
+
+// import js-cookie
+import Cookies from "js-cookie";
+
+// Hook untuk delete product
+export const useProductDelete = () => {
+  return useMutation({
+    // Mutation function untuk delete product
+    mutationFn: async (id: number) => {
+      // Ambil token dari cookies
+      const token = Cookies.get("token");
+
+      // Lakukan request DELETE ke endpoint API
+      const response = await Api.delete(`/api/admin/products/${id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      // Kembalikan response
+      return response.data;
+    },
+  });
+};
