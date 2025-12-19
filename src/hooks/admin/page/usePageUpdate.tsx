c9cb1184a50a33ea2e69911fdc2e9f5dcf6cafb1
panjiasmoroart <pandjiasmoroart@gmail.com>
2025-12-19 11:11:42 +0700

    Page Hooks - Membuat Hook Page Update (Mutation)

---
+++ src/hooks/admin/page/usePageUpdate.tsx
@@ -0,0 +1,31 @@
+// import useMutation dari '@tanstack/react-query';
+import { useMutation } from "@tanstack/react-query";
+
+// import service API
+import Api from "../../../services/api";
+
+//import type Page
+import type { PageUpdateRequest } from "../../../types/page";
+
+// import js-cookie
+import Cookies from "js-cookie";
+
+// Hook untuk update page
+export const usePageUpdate = () => {
+  return useMutation({
+    // Mutation function untuk update page
+    mutationFn: async (data: PageUpdateRequest) => {
+      // Ambil token dari cookies
+      const token = Cookies.get("token");
+
+      // Lakukan request PUT ke endpoint API
+      const response = await Api.put(`/api/admin/pages/${data.id}`, data, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      return response.data;
+    },
+  });
+};
