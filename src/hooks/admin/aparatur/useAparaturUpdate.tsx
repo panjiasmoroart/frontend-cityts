745ffdbc11fb8e174be3dffa3b0dd4c8bbff485a
panjiasmoroart <pandjiasmoroart@gmail.com>
2026-01-05 16:56:01 +0700

    Aparatur Hooks - Membuat Hook Aparatur Update (Mutation)

---
+++ src/hooks/admin/aparatur/useAparaturUpdate.tsx
@@ -0,0 +1,45 @@
+// import useMutation dari '@tanstack/react-query';
+import { useMutation } from "@tanstack/react-query";
+
+// import service API
+import Api from "../../../services/api";
+
+// import type
+import type { AparaturUpdateRequest } from "../../../types/aparatur";
+
+// import js-cookie
+import Cookies from "js-cookie";
+
+// Custom hook untuk update Aparatur
+export const useAparaturUpdate = () => {
+  return useMutation({
+    // Mutation function untuk update aparatur
+    mutationFn: async (data: AparaturUpdateRequest) => {
+      // Ambil token dari cookie
+      const token = Cookies.get("token");
+
+      // Buat FormData
+      const formData = new FormData();
+      if (data.image) {
+        formData.append("image", data.image);
+      }
+      formData.append("name", data.name);
+      formData.append("position", data.position);
+      formData.append("description", data.description);
+
+      // Kirim request PUT ke endpoint update aparatur
+      const response = await Api.put(
+        `/api/admin/aparaturs/${data.id}`,
+        formData,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+
+      return response.data;
+    },
+  });
+};
